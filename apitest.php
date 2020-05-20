d4621e892d814f378866f40f04b869cfaf962cce
Derek Henderson <derek.henderson@remote-learner.net>
2020-05-20 19:16:33 -0400

    WIP: Some updates to display the table, clean up some syntax issues, connection object.
    The database schema had to be adjusted to expand one column - its not included, as it seemed minor.

--- apitest.php
+++ apitest.php
@@ -1,4 +1,15 @@
 <?php
+// Connection object.
+$servername =  'localhost';
+$username = 'nautikos';
+$password  = 'carlin';
+$dbname = 'apitest';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+  
 $url = 'http://universities.hipolabs.com/search?country=canada';
 // Create a new cURL resource
 $ch = curl_init($url);
@@ -10,7 +21,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 $datacanada = json_decode($result, true);
 $country = 'Canada';
-$update = store_data($datacanada, $country);
+$update = store_data($datacanada, $country, $conn);
 $url = 'http://universities.hipolabs.com/search?country=us';
 
 $ch = curl_init($url);
@@ -22,56 +33,76 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 $dataus = json_decode($result, true);
 $country = 'US';
-$update = store_data($dataus, $country);
+$update = store_data($dataus, $country, $conn);
 
+echo '<table>';
+echo '<tr><td> Name</td><td>Number of websites</td><td>Number of Domains</td></tr>';
+
+
+$query = mysqli_query($conn,"SELECT ucname, iduniversity FROM  university order by ucname");
+while ($row = mysqli_fetch_array($query)) { 
+    $id = $row[1];
+    // Get the domain count;
+    $web = mysqli_query($conn, "SELECT * FROM  webpage where iduniverisity = ".$id); 
+    $webcount = mysqli_num_rows($web);
+    // Get the domain count;
+    $resultdomain = mysqli_query($conn, "SELECT * FROM  domains where iduniversity = ".$id); 
+    $domaincount = mysqli_num_rows($resultdomain);
+    $style1 = '<span style="color: red">';
+    if ($webcount > 1) {
+        $style1 = '<span style="color: green">';
+    }
+    $style2 = '<span style="color: blue">';
+    if ($domaincount > 1) {
+        $style2 = '<span style="color: orange">';
+    }
+
+	 echo '<tr><td>'.$row[0].'</td><td '.$style1.'>'.$webcount.'</span></td><td '.$style2.$domaincount.'</span></td></tr>';
+}
+echo '</table>';
 exit();
 
-Function store_data($data, $country) {
+Function store_data($data, $country, $conn) {
 // Function to store the data in the database;
-  $servername =  'localhost';
-  $username = 'nautikos';
-  $password  = 'carlin';
-  $dbname = 'apitest';
-
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
-  } 
   foreach ($data as $uc) {
       $alphacode = $uc['alpha_two_code'];
       $prov = $uc['state-province'];
       $ucname = $uc['name'];
+      $alphacode = mysqli_real_escape_string($conn, $alphacode);
+      $prov = mysqli_real_escape_string($conn, $prov);
+      $ucname = mysqli_real_escape_string($conn, $ucname);
       $wplist = $uc['web_pages'];
       $domains = $uc['domains']; 
-      $sql = "INSERT INTO university (alphacode, prov, ucname, country)
-              VALUES ($alphacode, $prov, $ucname, $country)";
+      $sql = "INSERT INTO university (alpha_code, prov, ucname, country)
+              VALUES ('$alphacode', '$prov', '$ucname', '$country')";
       
       if ($conn->query($sql) === TRUE) {
           $universityid = $conn->insert_id;
           foreach ($wplist as $wp) {
-             $sqlwp = "INSERT into webpage ('iduniversity', 'webpage') 
-                       VALUES ($universityid, $wp )";
-             if ($conn->query($sqlwp) === TRUE) {
-             } else {
-                echo "Error: " . $sqlwp . "<br>" . $conn->error;      
-                exit();
+             $wp = mysqli_real_escape_string($conn, $wp);
+
+             $sqlwp = "INSERT into webpage (iduniverisity, webpage) 
+                       VALUES ('$universityid', '$wp' )";
+             if (!$conn->query($sqlwp) === TRUE) {
+                 echo "Error: " . $sqlwp . "<br>" . $conn->error;      
+                 exit();
              }                               
           }
           foreach ($domains as $domain) {
-             $sqldomain = "INSERT into domains ('iduniversity', 'domainscol') 
-                       VALUES ($universityid, $domain )";
-             if ($conn->query($sqldomain) === TRUE) {
-             } else {
-                echo "Error: " . $sqldomain . "<br>" . $conn->error;      
-                exit();
+      	    $domain = mysqli_real_escape_string($conn, $domain);
+             $sqldomain = "INSERT into domains (iduniversity, domainscol) 
+                           VALUES ('$universityid', '$domain' )";
+             if (!$conn->query($sqldomain) === TRUE) {
+                 echo "Error: " . $sqldomain . "<br>" . $conn->error;      
+                 exit();
              }                               
           }
           
       } else{
-          echo "Error: " . $sql . "<br>" . $conn->error;      
+          echo "Error: " . $sql . "<br>" . $conn->error;
+          exit();      
       }
-      exit();    
    }
-      exit();
-   }
+   return true;
+}
   
